38b24a58115ec53205c4d796a3a7e4f3f6f7bce9
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-09-13 12:05:22 +0100

    re synthesized RVfpgaCopy

--- SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.runs/impl_1/rvfpganexys.tcl
+++ SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.runs/impl_1/rvfpganexys.tcl
@@ -17,7 +17,7 @@ proc create_report { reportName command } {
   }
 }
 namespace eval ::optrace {
-  variable script "C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/RVfpgaNexys_System/RVfpgaNexys_System.runs/impl_1/rvfpganexys.tcl"
+  variable script "C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.runs/impl_1/rvfpganexys.tcl"
   variable category "vivado_impl"
 }
 
@@ -130,16 +130,16 @@ OPTRACE "create in-memory project" START { }
   set_param project.singleFileAddWarning.threshold 0
 OPTRACE "create in-memory project" END { }
 OPTRACE "set parameters" START { }
-  set_property webtalk.parent_dir C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/RVfpgaNexys_System/RVfpgaNexys_System.cache/wt [current_project]
-  set_property parent.project_path C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/RVfpgaNexys_System/RVfpgaNexys_System.xpr [current_project]
-  set_property ip_output_repo C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/RVfpgaNexys_System/RVfpgaNexys_System.cache/ip [current_project]
+  set_property webtalk.parent_dir C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.cache/wt [current_project]
+  set_property parent.project_path C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.xpr [current_project]
+  set_property ip_output_repo C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "set parameters" END { }
 OPTRACE "add files" START { }
-  add_files -quiet C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/RVfpgaNexys_System/RVfpgaNexys_System.runs/synth_1/rvfpganexys.dcp
+  add_files -quiet C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/RVfpgaNexys_System/RVfpgaNexys_System.runs/synth_1/rvfpganexys.dcp
 OPTRACE "read constraints: implementation" START { }
-  read_xdc C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/src/rvfpganexys.xdc
-  read_xdc C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpga/src/LiteDRAM/liteDRAM.xdc
+  read_xdc C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/src/rvfpganexys.xdc
+  read_xdc C:/Users/Rui/Rui/MSc_Dissertation/SoC/RVfpgaCopy/src/LiteDRAM/liteDRAM.xdc
 OPTRACE "read constraints: implementation" END { }
 OPTRACE "add files" END { }
 OPTRACE "link_design" START { }
