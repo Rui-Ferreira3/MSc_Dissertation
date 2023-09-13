10f31e79d670c7bec5a68f83b43b0c651fd4eeae
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-09-13 16:39:49 +0100

    created cnn project for base soc

---
+++ SW_Projects/base/CNN/src/cnn.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#define DELAY 10000000
+
+void main (void){
+    int i, j = 0;
+
+    // Initialize UART
+    uartInit();
+
+    while (1) {
+        printfNexys("Hello world! Iteration: %d\n", j);
+        
+        for (i=0; i < DELAY; i++) ;  // delay between printf's
+        j++;
+    }
+}
